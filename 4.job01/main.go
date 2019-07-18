051ea1d565fc36ac7854dea096a7ab7bcdaf4c29
Lieman <15625000637@163.com>
2019-07-18 22:49:21 +0800

    day4 home

--- 4.job01/main.go
+++ 4.job01/main.go
@@ -2,27 +2,15 @@ package main
 
 import "fmt"
 
-//字符串倒序
-
-// s1 = "hello,世界"
-
-// func main(){
-// 	i = len(s1)
-// 	for k,_ := range s1{
-// 		k ,
-// 	}
-// }
-
-//
 func main() {
-	var a1 = make([]string, 5, 10)
-	var a2 = make([]int, 5, 10)
-	var b1 = make([]string, 2, 12)
-	var b2 = make([]int, 2, 12)
-	fmt.Println(len(a1))
-	fmt.Println(a2)
-	fmt.Println(b1)
-	fmt.Println(b2)
+	// var a1 = make([]string, 5, 10)
+	// var a2 = make([]int, 5, 10)
+	// var b1 = make([]string, 2, 12)
+	// var b2 = make([]int, 2, 12)
+	// fmt.Println(len(a1))
+	// fmt.Println(a2)
+	// fmt.Println(b1)
+	// fmt.Println(b2)
 
 	/*
 		输出：     切片string默认空字符    int 默认0
@@ -39,4 +27,13 @@ func main() {
 	// 	fmt.Println(a)
 	// }
 	// fmt.Println(a)
+	//字符串倒序输出
+	s1 := "hello"
+	b1 := []byte(s1) // [h e l l o]
+	s2 := ""
+	for i := len(b1) - 1; i >= 0; i-- {
+		s2 = s2 + string(b1[i])
+	}
+	fmt.Println(s2)
+
 }
